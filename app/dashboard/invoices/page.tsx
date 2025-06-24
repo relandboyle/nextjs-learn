e6c99ed7d3d25c8f5338c8d36e7cffe3ca2973f1
Reland <reland.boyle@gmail.com>
2025-06-24 18:08:32 -0400

    Chapter 7

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,15 @@
+'use client';
+
+import { useEffect } from "react";
+
 export default function Page() {
+
+  useEffect(() => {
+    // Example: Change the URL without reloading the page
+    window.history.pushState(null, 'New Title', '/dashboard');
+    // Or to replace the current history entry:
+    // window.history.replaceState(null, 'New Title', '/new-path-without-reload');
+  }, []);
+
   return <p>Invoices Page</p>;
 }
